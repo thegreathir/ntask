b7e87510deeb353d37b5b0d50feaf3b9d8dc8d26
thegreathir <aaph1377@gmail.com>
2023-05-10 19:15:04 +0330

    Add way filter based on highway tags

--- main.cpp
+++ main.cpp
@@ -1,20 +1,50 @@
 #include <iostream>
 #include <osmium/handler.hpp>
+#include <osmium/handler/node_locations_for_ways.hpp>
+#include <osmium/index/map/sparse_mem_array.hpp>
 #include <osmium/io/gzip_compression.hpp>
 #include <osmium/io/xml_input.hpp>
 #include <osmium/visitor.hpp>
+#include <vector>
 
 class DangerousBendHandler : public osmium::handler::Handler {
- public:
-  void way(const osmium::Way& way) { std::cout << way.id() << std::endl; }
+private:
+  const std::vector<std::string> way_tags;
+
+public:
+  DangerousBendHandler(const std::vector<std::string> &way_tags)
+      : way_tags(way_tags) {}
+
+  void way(const osmium::Way &way) {
+    if (way.tags().has_tag("oneway", "yes"))
+      return;
+    const auto *highway_value = way.tags().get_value_by_key("highway");
+    if (!highway_value)
+      return;
+    if (std::all_of(way_tags.cbegin(), way_tags.cend(),
+                    [highway_value](const std::string &way_tag) {
+                      return way_tag != highway_value;
+                    }))
+      return;
+
+    std::cout << way.id() << std::endl;
+  }
 };
 
 int main() {
-  osmium::io::File input_file{"west.osm.gz"};
-  osmium::io::Reader reader{input_file};
+  osmium::io::Reader reader{"west.osm.gz", osmium::osm_entity_bits::node |
+                                               osmium::osm_entity_bits::way};
 
-  DangerousBendHandler handler;
-  osmium::apply(reader, handler);
+  osmium::index::map::SparseMemArray<osmium::unsigned_object_id_type,
+                                     osmium::Location>
+      index;
+  auto location_handler = osmium::handler::NodeLocationsForWays{index};
+
+  const std::vector<std::string> way_tags = {"trunk", "primary", "secondary",
+                                             "tertiary"};
+  DangerousBendHandler dangerous_bend_handler{way_tags};
+
+  osmium::apply(reader, location_handler, dangerous_bend_handler);
   reader.close();
   return 0;
 }
