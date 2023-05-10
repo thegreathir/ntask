ea5d51dee461d1a83b26c6cc8f6f710e5e9aab07
thegreathir <aaph1377@gmail.com>
2023-05-10 18:11:32 +0330

    Add basic way explorer

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <osmium/handler.hpp>
+#include <osmium/io/gzip_compression.hpp>
+#include <osmium/io/xml_input.hpp>
+#include <osmium/visitor.hpp>
+
+class DangerousBendHandler : public osmium::handler::Handler {
+ public:
+  void way(const osmium::Way& way) { std::cout << way.id() << std::endl; }
+};
 
 int main() {
-  std::cout << "Hello" << std::endl;
+  osmium::io::File input_file{"west.osm.gz"};
+  osmium::io::Reader reader{input_file};
+
+  DangerousBendHandler handler;
+  osmium::apply(reader, handler);
+  reader.close();
   return 0;
 }
